3ba0f518e990e15611e258f5f5f772f391afa773
Antonio Dell'Annunziata <contact@antonio-da.dev>
2023-05-03 15:47:51 +0000

    ✨ feat(faq): Improve layout

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,29 +1,35 @@
 <template>
   <div class="container">
     <h1>FAQ</h1>
-    <p>
-      <strong>Wo kann ich den Newsletter abonnieren?</strong><br />
-      <NuxtLink to="/subscribe">HIER</NuxtLink> kannst du den Newsletter
-      abonnieren.
-    </p>
-    <p>
-      <strong>Wo kann ich den Newsletter abbestellen?</strong><br />
-      Du kannst den Newsletter
-      <NuxtLink to="/unsubscribe">HIER</NuxtLink> abbestellen.
-    </p>
-    <p>
-      <strong>Was passiert mit meinen Daten?</strong><br />
-      Alle Daten die für den Betrieb des Newsletters erhoben werden, werden
-      sicher in einer verschlüsselten Datenbank gespeichert und nicht an Dritte
-      zu irgendwelchen Zwecken weitergegeben. Sie werden ausschließlich zum
-      Zweck des Sendens von Newsletter-Emails mittels des Email-Services
-      <a href="https://sendgrid.com/">SendGrid</a> genutzt.
-      <br />
-      Für weitere Informationen zum SendGrid Anbieter Twilio, klicke
-      <a
-        href="https://www.twilio.com/en-us/legal/privacy#data-about-our-customers-end-users"
-        >HIER</a
-      >.
-    </p>
+    <section>
+      <h2>Wo kann ich den Newsletter abonnieren?</h2>
+      <p>
+        <NuxtLink to="/subscribe">HIER</NuxtLink> kannst du den Newsletter
+        abonnieren.
+      </p>
+    </section>
+    <section>
+      <h2>Wo kann ich den Newsletter abbestellen?</h2>
+      <p>
+        Du kannst den Newsletter
+        <NuxtLink to="/unsubscribe">HIER</NuxtLink> abbestellen.
+      </p>
+    </section>
+    <section>
+      <h2>Was passiert mit meinen Daten?</h2>
+      <p>
+        Alle Daten die für den Betrieb des Newsletters erhoben werden, werden
+        sicher in einer verschlüsselten Datenbank gespeichert und nicht an
+        Dritte zu irgendwelchen Zwecken weitergegeben. Sie werden ausschließlich
+        zum Zweck des Sendens von Newsletter-Emails mittels des Email-Services
+        <a href="https://sendgrid.com/">SendGrid</a> genutzt.
+        <br />
+        Für weitere Informationen zum SendGrid Anbieter Twilio, klicke
+        <a
+          href="https://www.twilio.com/en-us/legal/privacy#data-about-our-customers-end-users"
+          >HIER</a
+        >.
+      </p>
+    </section>
   </div>
 </template>
