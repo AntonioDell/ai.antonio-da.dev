c9d0350dc7ea78d00be8d778e3a25fed6d4b6591
Antonio Dell'Annunziata <contact@antonio-da.dev>
2023-05-11 20:42:06 +0000

    🚑 critical hotfix: Fix 500 precompile error on edge
    Hopefully only temporary until the default @nuxtjs/i18n can be used again.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n-edge"],
   runtimeConfig: {
     googleClientSecret: "",
     sendgridApiKey: "",
